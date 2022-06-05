af3d94cc292d45d39535e84d285654448094e9f7
EngSharaqi <muhammadsharaqi@gmail.com>
2022-06-05 12:26:57 +0200

    add main setup

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  
+  return (
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Web site created using create-react-app" />
+        {/* <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> --> */}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
+        {/* <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700;900&display=swap" rel="stylesheet">  */}
+        <link rel="icon" href="/favicon.ico" />
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+      </Head>
+      <Component {...pageProps} />    
+    </>
+    
+    );
+}
+
+export default MyApp
---
+++ pages/cosmetics.js
@@ -0,0 +1,51 @@
+import cosmeticsStyles from '../styles/cosmetics.module.css'
+var Cosmetics = () => {
+    return (
+        <>
+            <div className='row cosmetics'>
+                <div className='container'>
+                    <div className='row'>
+                    <div className='col-lg-3 col-md-4 col-sm-6 col-xs-12'>
+                            <div className='card'>
+                                <img src='https://placeholder.photo/img/1000x500' alt='' />
+                                <div>
+                                    <h4>Product Name</h4>
+                                    <p>Product details</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-3 col-md-4 col-sm-6 col-xs-12'>
+                            <div className='card'>
+                                <img src='https://placeholder.photo/img/1000x500' alt='' />
+                                <div>
+                                    <h4>Product Name</h4>
+                                    <p>Product details</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-3 col-md-4 col-sm-6 col-xs-12'>
+                            <div className='card'>
+                                <img src='https://placeholder.photo/img/1000x500' alt='' />
+                                <div>
+                                    <h4>Product Name</h4>
+                                    <p>Product details</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-3 col-md-4 col-sm-6 col-xs-12'>
+                            <div className='card'>
+                                <img src='https://placeholder.photo/img/1000x500' alt='' />
+                                <div>
+                                    <h4>Product Name</h4>
+                                    <p>Product details</p>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Cosmetics;
